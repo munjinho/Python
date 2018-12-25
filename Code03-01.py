90cf28e7e8ceac315d6279d01d205f871a100349
Jinho <mjh9082@gmail.com>
2018-12-26 02:07:05 +0900

    Python_Basic_Src

---
+++ Code03-01.py
@@ -0,0 +1,10 @@
+print("정수:%d" % 123)
+print("%5d" % 123)
+print("%05d" % 123)
+
+print("실수:%f" % 123.45)
+print("%7.1f" % 123.45)
+print("%7.3f" % 123.45)
+
+print("문자열:%s" % "Python")
+print("%10s" % "Python")
---
+++ Code03-04.py
@@ -0,0 +1,20 @@
+sel = int(input("입력 진수 결정(16/10/8/2) : "))
+num = input("값 입력 : ")
+
+if sel == 16 :
+    num10 = int(num, 16)
+    print("값: %d",num10)
+if sel == 10 :
+    num10 = int(num, 10)
+    print("값: %d",num10)
+if sel == 8 :
+    num10 = int(num, 8)
+    print("값: %d",num10)
+if sel == 2 :
+    num10 = int(num, 2)
+    print("값: %d",num10)
+    
+print("16진수 ==> ", hex(num10))
+print("10진수 ==> ", num10)
+print(" 8진수 ==> ", oct(num10))
+print(" 2진수 ==> ", bin(num10))
